b17b1975e91eac3283a35694baa54bc782026098
Steve Nono <ghistev@yahoo.fr>
2019-03-30 14:44:23 +0100

    The Game has 4 invisible walls preventing the ball from leaving the playing area

---
+++ Assets/BallControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Vector3 velocity;
+    public float maxZ;
+    public float maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(0, 0, maxZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.CompareTag("Flipper"))
+        {
+        Transform ball = transform;
+        Transform Flipper = other.transform;
+
+        float maxDistance = other.transform.localScale.x * 1 * 0.5f + transform.localScale.x * 1 * 0.5f;
+        float distance = ball.position.x - Flipper.position.x;
+        float nDistance = distance / maxDistance;
+
+        velocity = new Vector3(nDistance * maxX, velocity.y, -velocity.z);
+        }
+
+        if (other.CompareTag("Wall"))
+        {
+            velocity = new Vector3(-velocity.x, velocity.y, velocity.z);
+        }
+
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Assets/FlipperController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    public float speed;
+    public Transform PlayingSurface;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dir = 0;
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    dir = -1;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    dir = 1;
+        //}
+
+
+        dir = Input.GetAxis("Horizontal");
+        float newPositionX = transform.position.x + dir*speed*Time.deltaTime;
+//        float maxX= (0.5f * PlayingSurface.localScale.x * 10) - 0.5f * transform.localScale.x * 1;
+       // float clampedX = Mathf.Clamp(newPositionX, --maxX, maxX);
+        transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+
+    }
+}
